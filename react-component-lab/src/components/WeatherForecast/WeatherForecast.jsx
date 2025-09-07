73e87c3d9e9ab54d71ac8a5946a34c7ed2053d60
Pressturn <prestonyeo1@gmail.com>
2025-09-07 19:03:07 +0800

    Added react-component-lab

---
+++ react-component-lab/src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import './WeatherForecast.css';
+import WeatherData from './WeatherData';
+import WeatherIcon from './WeatherIcon';
+
+function WeatherForecast({ day, img, imgAlt, conditions, time }) {
+    return (
+        <div className="weather">
+            <WeatherData day={day} conditions={conditions} time={time}/>
+            <WeatherIcon img={img} imgAlt={imgAlt}/>
+        </div>
+    )
+}
+
+export default WeatherForecast;
