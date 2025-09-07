323489c34b249aebf6bfbb2bebcee33581330eaf
Pressturn <prestonyeo1@gmail.com>
2025-09-08 00:57:34 +0800

    Lab Done

--- react-state-management-lab/src/App.jsx
+++ react-state-management-lab/src/App.jsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
+import "./App.css"
+
+const App = () => {
+
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [zombieFighters, setZombieFighters] = useState([
+    {
+      id: 1,
+      name: 'Survivor',
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png',
+    },
+    {
+      id: 2,
+      name: 'Scavenger',
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png',
+    },
+    {
+      id: 3,
+      name: 'Shadow',
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png',
+    },
+    {
+      id: 4,
+      name: 'Tracker',
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
+    },
+    {
+      id: 5,
+      name: 'Sharpshooter',
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
+    },
+    {
+      id: 6,
+      name: 'Medic',
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
+    },
+    {
+      id: 7,
+      name: 'Engineer',
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
+    },
+    {
+      id: 8,
+      name: 'Brawler',
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
+    },
+    {
+      id: 9,
+      name: 'Infiltrator',
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
+    },
+    {
+      id: 10,
+      name: 'Leader',
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
+    },
+  ])
+
+  const totalStrength = team.reduce((sum, member) => sum + member.strength, 0);
+  const totalAgility = team.reduce((sum, member) => sum + member.agility, 0);
+
+  function handleAddFighter(fighter) {
+    if (money < fighter.price) {
+      console.log("Not Enough Money.");
+      return;
+    }
+    setTeam(prev => [...prev, fighter]);
+    setZombieFighters(zombieFighters => zombieFighters.filter(f => f.id !== fighter.id))
+    setMoney(currentAmount => currentAmount - fighter.price)
+  }
+
+  function handleRemoveFighter(fighter) {
+    setTeam(team => team.filter(f => f.id !== fighter.id));
+    setZombieFighters(zombieFighters => [...zombieFighters, fighter]);
+    setMoney(money => money + fighter.price);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h1>Zombie Fighters</h1>
+      <h2>Money: {money}</h2>
+      <h2>Team Strength: {totalStrength}</h2>
+      <h2>Team Agility: {totalAgility}</h2>
+      <h2>Team</h2>
+      {team.length === 0 ? (
+        <p>Pick Some Team Members!</p>
+      ) : (
+        <ul>
+          {team.map(member => (
+            <li key={member.id}>
+              <img src={member.img} alt={member.name} />
+              <p>Name: {member.name}</p>
+              <p>Price: {member.price}</p>
+              <p>Strength:{member.strength}</p>
+              <p>agility:{member.agility}</p>
+              <button onClick={() => handleRemoveFighter(member)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
 
-export default App
+      {zombieFighters.map((zombieFighter) => (
+        <p key={zombieFighter.id}>
+          <img src={zombieFighter.img} alt={zombieFighter.name} />
+          <p>{zombieFighter.name}</p>
+          <p>Price: {zombieFighter.price}</p>
+          <p>Strength: {zombieFighter.strength}</p>
+          <p>agility: {zombieFighter.agility}</p>
+          <button onClick={() => handleAddFighter(zombieFighter)}
+          >Add</button>
+        </p>
+      ))}
+    </>
+  );
+};
+
+export default App;
