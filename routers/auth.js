3d5ba20711ce68c73468567dcdbcbda3211e4147
Pressturn <prestonyeo1@gmail.com>
2025-10-25 00:24:59 +0800

    Remove duplicate files

--- routers/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signIn, signUp } = require("../controllers/auth");
-
-router.post("/sign-up", signUp);
-router.post("/sign-in", signIn);
-
-module.exports = router;
--- routers/ingredients.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { allIngredients, createIngredients } = require('../controllers/ingredients.js')
-
-
-router.get('/', allIngredients)
-router.post('/', createIngredients)
-
-module.exports = router
